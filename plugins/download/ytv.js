2281e81fb3052a59cd902ed5ed7eefe6e693b1f4
I'am Carlos <103902036+CarlosTwT@users.noreply.github.com>
2024-10-19 18:24:01 -0500

    ytmp4 fix + ytsearch

--- plugins/download/ytv.js
+++ plugins/download/ytv.js
@@ -46,6 +46,7 @@ name: 'youtubevideo',
   }
 }
 */
+import yts from 'yt-search';
 import { ytmp4 } from 'ruhend-scraper';
 
 export default {
@@ -55,21 +56,25 @@ export default {
   description: 'Descargar video de YouTube',
   example: Func.example('%p', '%cmd', 'https://youtu.be/MvsAesQ-4zA'),
   limit: true,
-  premium: false,
   run: async (m, { sock, text, prefix, command }) => {
-    if (!text) return m.reply(`Ingresa el enlace de YouTube ${m.prefix+command} https://youtu.be/MvsAesQ-4zA`);
+    if (!text) return m.reply(Func.example(m.prefix, command, 'spiral by longman'));
+    m.reply(global.status.wait);
     try {
-      const { title, video, author, description, duration, views, upload, thumbnail } = await ytmp4(text);
-      let caption = `[ YouTube Play ]*\n\n`;
-      caption += `*-* *Título* : ${title}\n`;
-      caption += `*-* *Autor* : ${author}\n`;
-      caption += `*-* *Descripción* : ${description}\n`;
-      caption += `*-* *duración* : ${duration}\n`;
-      caption += `*-* *título* : ${views}\n`;
-      caption += `*-* *Upload* : ${upload}`;
+      let yt = await (await yts(text)).all;
+      let old = new Date();
+
+      let ca = `*[ YouTube Play ]*\n\n`;
+      ca += `*-* *Título* : ` + yt[0].title + `\n`;
+      ca += `*-* *Duración* : ` + yt[0].timestamp + `\n`;
+      ca += `*-* *Viewer* : ` + yt[0].views + `\n`;
+      ca += `*-* *Hace* : ` + yt[0].ago + '\n';
+      ca += `*-* *Link* : ` + yt[0].url + `\n`;
+      ca += `*-* *Download* : 1-2 minutos\n\n`;
+      ca += global.set.footer;
+      const { video } = await ytmp4(yt[0].url);
       
-      await sock.sendFThumb(m.chat, global.set.wm, caption, thumbnail, text, m); 
-      await sock.sendMessage(m.chat, { document: { url: video },  mimetype: 'video/mp4', fileName: title + '.mp4' }, { quoted: m });
+      await sock.sendFThumb(m.chat, global.set.wm, ca, yt[0].thumbnail, yt[0].url, m); 
+      await sock.sendMessage(m.chat, { document: { url: video },  mimetype: 'video/mp4', fileName: yt[0].title + '.mp4' }, { quoted: m });
     } catch (error) {
       console.error(error);
       m.reply('Ocurrió un error al procesar el video. Por favor, intenta con otro enlace o más tarde.');
